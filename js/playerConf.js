a88487a0b4f11c591934ca4e2edc4a936641b28f
Oskaret27 <63423397+Oskaret27@users.noreply.github.com>
2020-04-16 16:40:38 +0200

    main button

--- js/playerConf.js
+++ js/playerConf.js
@@ -5,6 +5,14 @@ let textStyle = {
 	fill: '#b60404'
 };
 
+let textStyle2 = {
+	font: 'Cipitillo',
+	fontSize: '30pt',
+	fontWeight: 'bold',
+	fill: '#088A08'
+};
+
+
 let suggestedNames;
 
 let playerConfState = {
@@ -15,6 +23,7 @@ let playerConfState = {
 
 function preloadPlayerConf() {
 	game.load.image('rndButton', 'assets/buttons/rndButton.png');
+	game.load.image('backButton', 'assets/buttons/backButton.png');
 	game.load.image('bg2', 'assets/backgrounds/backgroundCastles.png');
 }
 
@@ -24,16 +33,23 @@ function createPlayerConf() {
 	let text = game.add.text(game.world.centerX, 80, 'Choose your name:', textStyle); // Text
 	text.anchor.setTo(0.5, 0.5);
 
-	let text2 = game.add.text(game.world.centerX, 500, 'Press Enter for accept', textStyle); // Text
+	let text2 = game.add.text(game.world.centerX, 400, 'Press button for random names', {
+		font: 'Rammetto One',
+		fontSize: '15pt',
+		fill: '#b60404'
+	}); // Text
 	text2.anchor.setTo(0.5, 0.5);
 
-	playerName = game.add.text(game.world.centerX, 130, "", textStyle); // Text del nom del jugador
+	playerName = game.add.text(game.world.centerX, 170, "", textStyle2); // Text del nom del jugador
 	playerName.anchor.setTo(0.5, 0.5);
 
-	rndButton = game.add.button(game.world.width / 2, game.world.height / 2, 'rndButton', onButtonPressed);
+	rndButton = game.add.button(game.world.centerX, 300, 'rndButton', onButtonPressed);
 	rndButton.anchor.setTo(0.5, 0.5);
 	rndButton.scale.setTo(0.6, 0.6);
 
+	let btnBack = game.add.button(game.world.width / 2, game.world.height - 60, 'backButton', backMain);
+	btnBack.anchor.setTo(0.5, 0.5);
+	btnBack.scale.setTo(1.2, 1.2);
 
 	let callbackContext = game.input.keyboard.callbackContext; // callback context, necesari per a definir les callbacks
 	game.input.keyboard.addCallbacks(callbackContext, onDown); // Definim les callbacks (onDown [, onUp, onPress])
@@ -58,9 +74,8 @@ function onDown() { // Se crida cuan una key està down
 
 	if (game.input.keyboard.event.key == "Backspace") { // Li donen al la key de borrar
 		playerNameStr = playerNameStr.substring(0, playerNameStr.length - 1);
-	} else if (game.input.keyboard.event.key == "Enter") { // Li donen al enter
-		game.state.start('menu');
-	} else if (game.input.keyboard.event.key.length == 1) { // Cualsevol lletra "normal" (caracters especials tamé)
+	} 
+	else if (game.input.keyboard.event.key.length == 1) { // Cualsevol lletra "normal" (caracters especials tamé)
 		playerNameStr += game.input.keyboard.event.key;
 	}
 
@@ -68,10 +83,14 @@ function onDown() { // Se crida cuan una key està down
 }
 
 
+function backMain() {
+	if (playerName.text.length > 0){
+		game.state.start('menu');}				
+}
+
+
 function onButtonPressed(button) {
 
 	playerName.setText(suggestedNames[Math.floor(Math.random() * 10)]); // Actualitza el text de pantalla
 	
-	//game.state.start('menu', menuState);
-
 }
